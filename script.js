877aec63b97d1f9fb6f588552ec8c8c57b423c55
Gabriel Perez <gabrielrperez.dev@gmail.com>
2024-01-03 06:18:55 -0500

    higher-order functions

--- script.js
+++ script.js
@@ -26,3 +26,47 @@
 
 // newPassport(gabe);
 // checkIn(flight, gabe);
+
+// const oneWord = function (str) {
+//   return str.replace(/ /g, "").toLowerCase();
+// };
+
+// const upperFirstWord = function (str) {
+//   const [first, ...others] = str.split(" ");
+//   return [first.toUpperCase(), ...others].join(" ");
+// };
+
+// const transformer = function (str, fn) {
+//   console.log(`Original string ${str}`);
+//   console.log(`Transformed string ${fn(str)}`);
+//   console.log(`Trasnformed by: ${fn.name}`);
+// };
+
+// transformer("Javascript is the best", oneWord);
+
+// const passenger = {
+//   name: "Gabriel Perez",
+//   nation: "USA",
+// };
+
+// const passenger2 = {
+//   name: "Gabriel Perez",
+//   nation: "Turkey",
+// };
+
+// const isAmerican = function (passenger) {
+//   if (passenger.nation === "USA") {
+//     console.log("Is American");
+//     return true;
+//   }
+// };
+
+// const entry = function (passenger, fn) {
+//   if (fn(passenger)) {
+//     console.log(`American, please enter`);
+//   } else {
+//     console.log(`Proceed to Passport check`);
+//   }
+// };
+
+// entry(passenger2, isAmerican);
