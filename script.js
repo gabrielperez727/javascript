c0a233a08eb5d128a6b3c1d69a1af77adc7fbd93
Gabriel Perez <gabrielrperez.dev@gmail.com>
2024-01-05 01:35:31 -0500

    closures

--- script.js
+++ script.js
@@ -143,3 +143,43 @@
 // };
 
 // console.log(addVAT(100, addTax));
+
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+g();
+f();
+h();
+f();
+
+//
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+boardPassengers(180, 3);
+
+(function () {
+  const header = document.querySelector("h1");
+  header.style.color = "red";
+  document.querySelector("applet").addeventlistener("click", function () {
+    header.style.color = "blue";
+  });
+})();
